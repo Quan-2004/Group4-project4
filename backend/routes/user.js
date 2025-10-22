0b40cfb56f92d9662066de3b757176778455d89e
Tấn Đạt Trần <trandatdat7388@gmail.com>
2025-10-22 16:29:02 +0700

    sua put and delete

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,5 +5,7 @@ const ctrl = require('../controllers/userController');
 
 router.get('/users', ctrl.getUsers);
 router.post('/users', ctrl.createUser);
+router.put('/users/:id', ctrl.updateUser);
+router.delete('/users/:id', ctrl.deleteUser);
 
 module.exports = router;
